823582a27ba837d2265bc819c00801412bd38f93
Daniel Lang <dkllang1@gmail.com>
2026-01-03 09:54:34 -0600

    convert to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import db from "@astrojs/db";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react(), db()],
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  integrations: [react()],
 });
