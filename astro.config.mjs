e25a18df6f9f744aee5b1f67e664e1d7fe8da137
Daniel Lang <dkllang1@gmail.com>
2026-07-09 13:35:58 -0500

    upgrading astro, bumping version to 2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -12,5 +11,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react(), icon()],
+  integrations: [icon()],
 });
